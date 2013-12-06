ec4ddf6030847f1ac93581ef4ce0271587687587
Masafumi Yokoyama <myokoym@gmail.com>
2013-12-06 23:41:07 +0900

    Remove needless new lines

--- lingrbot.rb
+++ lingrbot.rb
@@ -22,9 +22,7 @@ post "/" do
   when /shogikoma/
     return "shogikoma: it supports one or two characters." if command_params.length > 2
     image_uri = File.join("shogikoma", "#{Time.now.strftime("%Y%m%d%H%M%S")}.png")
-    output_path = File.join(File.dirname(__FILE__), 
-                            "public",
-                            image_uri)
+    output_path = File.join(File.dirname(__FILE__), "public", image_uri)
     painter = ShogiKoma::Painter.new
     painter.width = 200
     painter.height = 200
