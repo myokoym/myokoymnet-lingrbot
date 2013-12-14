b13b29e282c49db8714280759e62e64bfdb2a82c
Masafumi Yokoyama <myokoym@gmail.com>
2013-12-14 23:43:23 +0900

    test: simplify addition of a new test

--- test/test-lingrbot.rb
+++ test/test-lingrbot.rb
@@ -5,26 +5,30 @@ class LingrbotTest < Test::Unit::TestCase
     @app ||= Rack::Builder.parse_file(File.join(base_dir, "config.ru")).first
   end
 
+  def setup
+    @request = File.read(File.join(fixtures_dir, "test-request.json"))
+  end
+
   def test_parse
-    post "/", File.read(File.join(fixtures_dir, "test_parse.json"))
+    post "/", @request
     assert_true(last_response.ok?)
     assert_empty(last_response.body)
   end
 
   def test_mecab
-    post "/", File.read(File.join(fixtures_dir, "test_mecab.json"))
+    post "/", @request.gsub("XXX", "%25mecab hoge")
     assert_true(last_response.ok?)
     assert_match(/\Ahoge\t.*(,.*){5}\n\z/, last_response.body)
   end
 
   def test_mora
-    post "/", File.read(File.join(fixtures_dir, "test_mora.json"))
+    post "/", @request.gsub("XXX", "%25mora foo")
     assert_true(last_response.ok?)
     assert_equal("0", last_response.body)
   end
 
   def test_shogikoma
-    post "/", File.read(File.join(fixtures_dir, "test_shogikoma.json"))
+    post "/", @request.gsub("XXX", "%25shogikoma R")
     assert_true(last_response.ok?)
     assert_match(%r(\Ahttp://myokoym.net/lingrbot/shogikoma/\w+\.png\z),
                  last_response.body)
